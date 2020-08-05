7f1d4b1cc46c39c2b92c88f2fba61b3ef59ecc12
Spencer Smith <robertspencersmith@gmail.com>
2020-08-05 12:24:17 -0400

    fix: ensure we don't clobber extra args when trying to label nodes
    Signed-off-by: Spencer Smith <robertspencersmith@gmail.com>

--- main.go
+++ main.go
@@ -254,7 +254,9 @@ func FetchConfig(w http.ResponseWriter, r *http.Request) {
 				if _, ok := config.MachineConfig.MachineKubelet.KubeletExtraArgs["node-labels"]; ok {
 					config.MachineConfig.MachineKubelet.KubeletExtraArgs["node-labels"] += fmt.Sprintf(",metal.arges.dev/uuid=%s", serverObj.Name)
 				} else {
-					config.MachineConfig.MachineKubelet.KubeletExtraArgs = make(map[string]string)
+					if config.MachineConfig.MachineKubelet.KubeletExtraArgs == nil {
+						config.MachineConfig.MachineKubelet.KubeletExtraArgs = make(map[string]string)
+					}
 					config.MachineConfig.MachineKubelet.KubeletExtraArgs["node-labels"] = fmt.Sprintf("metal.arges.dev/uuid=%s", serverObj.Name)
 				}
 
